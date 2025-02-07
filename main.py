71caf08e50d7c3ebfc9a38694fc619c0e449656e
devfemibadmus <devfemibadmus@gmail.com>
2025-02-07 09:59:14 +0100

    🚑️ a 🌍 pi 🎨

---
+++ main.py
@@ -0,0 +1,50 @@
+import os, sys, uvicorn, time
+from fastapi import FastAPI, Depends, Header, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_headers=["*"],
+    allow_CREDENTIALS=True,
+    allow_methods=["GET", "POST"],
+)
+PROCESSING = False
+RATES2NGN = {}
+CREDENTIALS = [
+    "uhmmmmmmmm",
+    "uhmmmmmmmmm",
+]
+
+def get_api_key(api_key: str = Header(...)):
+    if api_key not in CREDENTIALS:
+        raise HTTPException(status_code=403, detail="Invalid API key")
+    return api_key
+
+
+@app.post("/update/keys")
+def update_kyes(valid_keys: dict):
+    for key in list(RATES2NGN.keys()):
+        if key not in valid_keys['keys']:
+            del RATES2NGN[key]
+
+@app.post("/update/rate")
+def set_value(data: dict, api_key: str = Depends(get_api_key)):
+    if api_key in CREDENTIALS:
+        for key, value in data.items():
+            RATES2NGN[key] = value
+    return RATES2NGN if RATES2NGN else {}
+
+@app.get("/{key}")
+async def get_value(key: str):
+    value = RATES2NGN.get(key)
+    return value
+
+@app.get("/")
+async def get_data(key: str):
+    return storage
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
