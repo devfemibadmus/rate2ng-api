98cd15a6485e27aaff377df4fec73adbeca62020
devfemibadmus <devfemibadmus@gmail.com>
2025-02-09 01:08:44 +0100

    🔽 fix some and k 🔥 eep connection 👷

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
 from fastapi import FastAPI, BackgroundTasks
 import requests, time, uvicorn, asyncio, threading, signal, websockets, json
+from concurrent.futures import ThreadPoolExecutor
+
+executor = ThreadPoolExecutor(max_workers=10)
 
 app = FastAPI()
 stop_flag = threading.Event()
 
-host = "http://127.0.0.1:8000"
+host = "127.0.0.1:8000"
 api_key = "uhmmmmmmmmmmmmmmmmmmmm"
 currencies = {
     "USD-NGN": {},
@@ -20,7 +23,7 @@ currencies = {
     "JPY-NGN": {},
 }
 
-async def update_rates_from_google_finance(ticker):
+def update_rates_from_google_finance(ticker):
     while not stop_flag.is_set():
         url = f"https://www.google.com/finance/quote/{ticker}"
         headers = {"User-Agent": "Mozilla/5.0"}
@@ -29,19 +32,19 @@ async def update_rates_from_google_finance(ticker):
         price = soup.find("div", class_="YMlKec fxKbKc").text
         previous_close = soup.find("div", class_="gyFHrc").find("div", class_="P6K39c").text
         currencies[ticker]["price"] = {"current": price, "last24hrs": previous_close}
-        await asyncio.sleep(5)
+        time.sleep(5)
 
 async def update_rates():
-    while not stop_flag.is_set():
-        async with websockets.connect(f"{host}/update/currencies", extra_headers={"api_key": api_key}) as websocket:
+    async with websockets.connect(f"ws://{host}/update/rates", extra_headers={"api_key": api_key}) as websocket:
+        while not stop_flag.is_set():
             await websocket.send(json.dumps(currencies))
-        await asyncio.sleep(2)
+            await asyncio.sleep(2)
 
 @app.get("/")
 def set_rates(background_tasks: BackgroundTasks):
     if not stop_flag.is_set():
         for ticker in currencies.keys():
-            background_tasks.add_task(update_rates_from_google_finance, ticker)
+            executor.submit(update_rates_from_google_finance, ticker)
         background_tasks.add_task(update_rates)
     return "started!"
 
